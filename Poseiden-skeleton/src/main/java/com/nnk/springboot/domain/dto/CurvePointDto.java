9e684df684568391df451b8b4a9a5626303c2409
Mathieu Blanc <94637553+Blmat@users.noreply.github.com>
2022-12-29 13:54:56 +0100

    SecurityConfig + dto

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/dto/CurvePointDto.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/dto/CurvePointDto.java
@@ -4,14 +4,16 @@ import com.nnk.springboot.validation.IsDecimal;
 import com.nnk.springboot.validation.IsInteger;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class CurvePointDto {
 
     // user interface functionality
-
     private Integer id;
 
     @IsInteger
+    @NotNull(message = "must not be null")
     private String curveId;
 
     @IsDecimal
